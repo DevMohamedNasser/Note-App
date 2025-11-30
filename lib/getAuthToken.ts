a35ac29310054e18aebf927318a405900d29e899
devMohamedNasser <m.nasser.dev@gmail.com>
2025-12-01 00:12:31 +0200

    changes in token production

--- lib/getAuthToken.ts
+++ lib/getAuthToken.ts
@@ -2,8 +2,13 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getAuthToken() {
-    const authToken = (await cookies()).get('next-auth.session-token')?.value;
+
+    const cookieStore = await cookies();
+    const authToken = cookieStore.get('next-auth.session-token')?.value || cookieStore.get('__Secure-next-auth.session-token')?.value;
+
     const token = await decode({token: authToken, secret: process.env.NEXTAUTH_SECRET!});
+
+
     // console.log("=======================> Token", token);
     return token?.accessToken;
 }
